53bb37afd9d2908785696f8b134072913c0aaa3c
Clément Menou <clementmenou0@gmail.com>
2024-06-27 01:36:19 +0200

    migration fix + User model setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->unique();
+            $table->string('password')->nullable();
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
